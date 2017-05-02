6ae4506a893ea4f6c91030a97824eb0dd2b6fdd5
Pepe Valdivia <jvaldivia@softweb.pe>
2017-05-02 18:08:15 -0500

    Envío por POST usando query params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
 
 import { LoginPage } from '../pages/login/login';
 import { AboutPage } from '../pages/about/about';
@@ -25,7 +26,8 @@ import { Auth } from '../providers/auth';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Http, Headers } from '@angular/http';
 
 /**
  * Generated class for the Login page.
@@ -13,12 +15,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+    loginForm: FormGroup;
+    placeholder_usuario : string;
+    placeholder_contrasenia : string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public http:Http) {
+      this.placeholder_usuario = "Usuario";
+      this.placeholder_contrasenia ="Contraseña";
+      this.loginForm = formBuilder.group({
+        usuario: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        contrasenia: ['']
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Login');
+  }
+
+  public validar(usuario, contrasenia){
+     let headers = new Headers();
+     headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+     var x = "quinua";
+     var y = "smart"; 
+     var params  = "usuario=" + x + "&contrasenia=" + y;
+     this.http.post("http://localhost:3000/login", params, {headers : headers}).map(res => res.json()).subscribe(data => {
+        console.log(data);
+     });
+      //alert(usuario);
+      //alert(contrasenia);
   }
 
 }
